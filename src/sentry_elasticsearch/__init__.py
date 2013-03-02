4f58b53b0302ae6361660931f40d3ce37024101c
Federico Maggi <federico.maggi@gmail.com>
2013-03-02 12:12:48 +0100

    Changed importpath

--- src/sentry_elasticsearch/__init__.py
+++ src/sentry_elasticsearch/__init__.py
@@ -2,6 +2,6 @@
 
 try:
     VERSION = __import__('pkg_resources') \
-        .get_distribution(__name__).version
+        .get_distribution('sentry-elasticsearch').version
 except Exception, e:
     VERSION = 'unknown'
